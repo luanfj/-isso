77e0d43c508662a099d87890f5f09b5a07ed06d1
Luan Fernandes <49419439+Grawlk@users.noreply.github.com>
2019-08-27 09:19:48 -0300

    Update Command.js

--- src/struct/Command.js
+++ src/struct/Command.js
@@ -2,18 +2,20 @@ class Command {
   constructor(options = {}) {
     this.name = options.name;
     this.aliases = options.aliases || [];
-    this.category = options.category || "None";
+    this.category = options.category;
+    
+    this.onlyOwner = options.onlyOwner || false;
+		this.memberPermissions = options.memberPermissions || [];
+		this.botPermissions = options.botPermissions || [];
   }
-  
+
   register(client) {
     this.client = client;
     
     this.client.commands.push(this);
   }
   
-  async start(message) {
-    const prefix = "bot!";
-    
+  async execute(message, prefix) {
     const rawArgs = message.content.split(" ");
     
     let names = [this.name];
@@ -22,17 +24,41 @@ class Command {
       names.push(alias);
     });
     
-    const find = names.map(command => prefix + command);
+    const commands = names.map(command => prefix + command);
     
-    if(find.includes(rawArgs[0])) {
+    if(commands.includes(rawArgs[0])) {
       try {
+        const missingMemberPermissions = this.memberPermissions.filter((permission) => !message.member.hasPermission(permission));
+        
+        if (missingMemberPermissions.length !== 0) {
+					message.reply("você não tem permissão para executar este comando!");
+					return true;
+				}
+        
+        const missingBotPermissions = this.botPermissions.filter((permission) => !message.guild.me.hasPermission(permission));
+
+				if (missingBotPermissions.length !== 0) {
+					message.reply("Eu não consigo executar este comando pois eu preciso das permissões: __**" + missingBotPermissions.join(", ") + "**__!");
+					return true;
+				}
+        
         const args = rawArgs;
         args.shift();
         
-        await this.run({message, args});
-      } catch (e) {}
+        await this.run({ message, args });
+      } catch(e) {}
     }
+  }
+  
+  loadingEmbed(message, RichEmbed) {
+    const embed = new RichEmbed()
+      .setTitle(message.guild.name, message.guild.iconURL)
+      .setDescription("<a:loading:615520808424570901> Aguarde um momento...")
+      .setColor(0x36393e)
+      .setFooter(message.author.username, message.author.avatarURL)
+    
+    return message.channel.send(embed);
   }
 }
 
-module.exports = Command;
+export default Command;
