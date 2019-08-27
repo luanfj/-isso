26865e43de06bc5e2ee69a592d0cd7943c511713
Luan Fernandes <49419439+Grawlk@users.noreply.github.com>
2019-08-27 09:10:11 -0300

    Update ping.js

--- src/commands/status/ping.js
+++ src/commands/status/ping.js
@@ -1,17 +1,26 @@
-const Command = require("../../struct/Command");
+import Command from '../../struct/Command';
+import { RichEmbed } from 'discord.js';
 
-class PingCommand extends Command {
+class Ping extends Command {
   constructor() {
     super({
-      name: "ping",
-      aliases: ['p'],
-      category: "status"
+      name: "ping"
     });
   }
-  
-  run({message}) {
-    message.channel.send(`API: ${Math.floor(this.client.ping)}ms / Messagem: ${Math.floor(Date.now() - message.createdTimestamp)}ms`);
+
+  run({ message, args }) {
+    const embed = new RichEmbed()
+      .setAuthor(message.guild.name, message.guild.iconURL)
+      .addField("Latency", `${Math.floor(Date.now() - message.createdTimestamp)}ms`, true)
+      .addField("API Latency", `${Math.floor(this.client.ping)}ms`, true)
+      .setColor(0x36393f)
+      .setFooter(message.author.username, message.author.avatarURL)
+      .setTimestamp()
+    
+    this.loadingEmbed(message, RichEmbed).then(msg => {
+      msg.edit(embed);
+    });
   }
 }
 
-module.exports = PingCommand;
+export default Ping;
